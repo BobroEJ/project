6a7612d605adc68405a2a52cc76e321dad2f6ecd
Evgeniy <tverdune@ya.ru>
2021-01-06 18:05:44 +0300

    Условия

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 'use strict';
 
-const numberOfFilms = +prompt('how much?', '');
+if (4 == 9) {
+    console.log('ok!');
+} else {
+    console.log('error!');
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-console.log(personalMovieDB.count);
 
-const a = prompt('film1?', ''),
-      b = +prompt('rating', ''),
-      c = prompt('film2?', ''),
-      d = +prompt('rating', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Ого!');
+// } else {
+//     console.log('Ok!');
+// }
 
-console.log(personalMovieDB);
+// (num === 50) ? console.log('Ok!') : console.log('Error!');
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('49');
+        break;
+    case 100:
+        console.log('100');
+        break;
+    case 50:
+        console.log('OK');
+        break;
+    default:
+        console.log('Бывает');
+        break;
+
+}
