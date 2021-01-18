2ded7dcaf8e29b664fdea3a0a616237c31f0dc0c
Evgeniy <tverdune@ya.ru>
2021-01-18 17:41:48 +0300

    045 Функции-конструкторы

---
+++ exp/045/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+User.prototype.exit = function() {
+    console.log(`Bye ${this.name}`);
+}
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+console.log(ivan, alex);
+
+ivan.hello()
+alex.hello()
+ivan.exit()
