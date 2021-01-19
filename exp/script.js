16c026226816b62f11fcefabb5007de54569a75b
Evgeniy <tverdune@ya.ru>
2021-01-19 16:26:32 +0300

    051 JSON формат передачи данных, глубокое клонирование объектов

--- exp/script.js
+++ exp/script.js
@@ -1,24 +1,15 @@
 'use strict';
 
-const box = document.querySelector('.box'),
-      btn = document.querySelector('button');
+const person = {
+    name: 'Alex',
+    tel: '+74444444444',
+    parents: {
+        mom: 'Nina',
+        dad: 'Vova'
+    }
+};
 
-// const width = box.clientWidth;
-// const height = box.clientHeight;
-// const width = box.offsetWidth;
-// const height = box.offsetHeight;
-const width = box.scrollWidth;
-const height = box.scrollHeight;
-
-console.log(width, height);
-
-btn.addEventListener('click', () => {
-    // box.style.height = box.scrollHeight + 'px';
-    console.log(box.scrollTop);
-})
-
-console.log(box.getBoundingClientRect().top);
-
-const style = window.getComputedStyle(box);
-
-console.log(style.display);
+const clone = JSON.parse(JSON.stringify(person));
+clone.name = 'Zhenya';
+console.log(person);
+console.log(clone);
