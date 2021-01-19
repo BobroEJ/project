2c04bbf6cdf75f86cea3972e96ca786c9d369b49
Evgeniy <tverdune@ya.ru>
2021-01-19 17:36:37 +0300

    052 AJAX и общение с сервером

---
+++ exp/052/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type','applicarion/json; charset=UTF-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = `Что-то пошло не так...`;
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+});
